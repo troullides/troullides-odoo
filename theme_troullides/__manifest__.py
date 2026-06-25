47e70a0a928ea4dbd17adc819ae4bb51f7d35020
Troullides <info@troullides.net>
2026-06-25 12:04:01 +0000

    Add initial Troullides theme module

---
+++ theme_troullides/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Troullides Website Theme",
+    "version": "19.0.1.0.0",
+    "category": "Website",
+    "summary": "Troullides custom website styling",
+    "depends": ["website", "website_sale"],
+    "assets": {
+        "web.assets_frontend": [
+            "theme_troullides/static/src/css/troullides.css",
+        ],
+    },
+    "installable": True,
+    "application": False,
+}
