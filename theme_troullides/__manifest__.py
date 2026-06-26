f634cf82d1999a277f46facdb9cef6b64cf987d2
Stathis Troullides <stathis@troullides.com.cy>
2026-06-26 11:11:35 +0300

    Add homepage template

--- theme_troullides/__manifest__.py
+++ theme_troullides/__manifest__.py
@@ -3,12 +3,21 @@
     "version": "19.0.1.0.0",
     "category": "Website",
     "summary": "Troullides custom website styling",
-    "depends": ["website", "website_sale"],
+    "depends": [
+        "website",
+        "website_sale",
+    ],
+
+    "data": [
+        "views/homepage.xml",
+    ],
+
     "assets": {
         "web.assets_frontend": [
             "theme_troullides/static/src/css/troullides.css",
         ],
     },
+
     "installable": True,
     "application": False,
 }
