869dfa01d5589deff464ebd66520cef09f01920d
Stathis Troullides <stathis@troullides.com.cy>
2026-06-27 12:31:10 +0300

    Add Troullides website module manifest

---
+++ troullides_website/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Troullides Website",
+    "version": "19.0.1.0.0",
+    "category": "Website",
+    "summary": "Custom Troullides website pages",
+    "depends": [
+        "website",
+        "website_sale",
+        "theme_troullides",
+    ],
+    "data": [
+        "views/homepage.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
