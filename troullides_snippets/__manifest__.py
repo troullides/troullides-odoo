5303342d55832e4ca526074e0ec95ba770fb4eef
Stathis Troullides <stathis@troullides.com.cy>
2026-06-28 11:34:35 +0300

    Add Troullides snippets module

---
+++ troullides_snippets/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "author": "Troullides",
+"website": "https://troullides.net",
+"license": "LGPL-3",
+    "name": "Troullides Website Snippets",
+    "version": "19.0.1.0.0",
+    "depends": [
+        "website",
+        "website_sale",
+        "theme_troullides",
+    ],
+    "data": [
+        "views/snippets.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
