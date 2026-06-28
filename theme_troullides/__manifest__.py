66c4ca6e5fc23afe7e9a20326306150e7e9685ec
Stathis Troullides <stathis@troullides.com.cy>
2026-06-28 13:24:29 +0300

    Remove homepage XML from theme manifest

--- theme_troullides/__manifest__.py
+++ theme_troullides/__manifest__.py
@@ -7,7 +7,7 @@
     "website": "https://troullides.net",
     "license": "LGPL-3",
     "depends": ["website", "website_sale"],
-    "data": ["views/homepage.xml"],
+    "data": [],
     "assets": {
         "web.assets_frontend": [
             "theme_troullides/static/src/css/troullides.css"
